bfcc9b7046acf65d59dedf23f0cd8edab7a14dbb
ollieshmollie <oliverduncan@icloud.com>
2016-12-24 23:57:59 -0600

    Draw basic jQuery animations

--- ajax.js
+++ ajax.js
@@ -1,3 +1,20 @@
+$.fn.extend({
+  slideFromLeft: function(html, success=null) {
+    return this.each(function() {
+      $(this).css({right: '0'})
+      $(this).animate({right: '-100%'}, function() {
+        $(this).removeAttr('style')
+        $(this).css({left: '-100%'})
+        $(this).html(html)
+        $(this).animate({left: '0'}, function() {
+          $(this).removeAttr('style')
+          if (success) success()
+        })
+      })
+    })
+  }
+})
+
 var $main = $('main')
 
 $(function() {
@@ -6,8 +23,7 @@ $('#tact').click(function(e) {
   e.preventDefault()
   $.get('ajax/tact/README.html', function(html) {
     console.log("Clicked tact")
-    $main.empty
-    $main.html(html)
+    $main.slideFromLeft(html)
   })
 })
 
@@ -15,12 +31,12 @@ $('#shnake').click(function(e) {
   e.preventDefault()
   $.get('ajax/shnake/index.html', function(html) {
     console.log("Clicked shnake")
-    $main.empty
-    $main.html(html)
-    $.getScript('ajax/shnake/serpent.js', function() {
-      $.getScript('ajax/shnake/game.js', function() {
-        $.getScript('ajax/shnake/play.js', function() {
-          console.log("Running shnake scripts...")
+    $main.slideFromLeft(html, function() {
+      $.getScript('ajax/shnake/serpent.js', function() {
+        $.getScript('ajax/shnake/game.js', function() {
+          $.getScript('ajax/shnake/play.js', function() {
+            console.log("Running shnake scripts...")
+          })
         })
       })
     })
