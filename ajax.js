dc98bfd0d1361e01c7b4155b751f3891234fe05d
ollieshmollie <oliverduncan@icloud.com>
2016-12-25 13:00:19 -0600

    Create responsive dropdown menu for mobilefirst design

--- ajax.js
+++ ajax.js
@@ -1,30 +1,19 @@
-$.fn.extend({
-  slideFromLeft: function(html, success=null) {
-    return this.each(function() {
-      $(this).css({right: '0'})
-      $(this).animate({right: '-100%'}, function() {
-        $(this).empty()
-        $(this).removeAttr('style')
-        $(this).css({left: '-100%'})
-        $(this).html(html)
-        $(this).animate({left: '0'}, function() {
-          $(this).removeAttr('style')
-          if (success) success()
-        })
-      })
-    })
-  }
+$(function() {
+
+var $menuIcon = $('i')
+var $menu = $('aside')
+$menuIcon.click(function() {
+  $menu.slideToggle('slow')
 })
 
 var $main = $('main')
-
-$(function() {
 
 $('#tact').click(function(e) {
   e.preventDefault()
   $.get('ajax/tact/README.html', function(html) {
     console.log("Clicked tact")
-    $main.slideFromLeft(html)
+    $menu.slideToggle()
+    $main.html(html)
   })
 })
 
@@ -32,18 +21,17 @@ $('#shnake').click(function(e) {
   e.preventDefault()
   $.get('ajax/shnake/index.html', function(html) {
     console.log("Clicked shnake")
-    $main.slideFromLeft(html, function() {
-      $.getScript('ajax/shnake/serpent.js', function() {
-        $.getScript('ajax/shnake/game.js', function() {
-          $.getScript('ajax/shnake/play.js', function() {
-            console.log("Running shnake scripts...")
-            game.play();
-          })
+    $menu.slideToggle()
+    $main.html(html)
+    $.getScript('ajax/shnake/serpent.js', function() {
+      $.getScript('ajax/shnake/game.js', function() {
+        $.getScript('ajax/shnake/play.js', function() {
+          console.log("Running shnake scripts...")
+          game.play();
         })
       })
     })
   })
 })
-
 
 })
